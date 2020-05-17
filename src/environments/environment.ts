cea2e05a67ec47641cdcca810fc951bbfc7b4d46
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-05-17 17:29:36 +0800

    environment for woocommerce fetch api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://apitaki.shadowsonawall.net/',
+  wooPath: '',
+  consumerKey: 'ck_e69ffab389c5ab9957b0f3e67a0398047f9d62d9',
+  consumerSecret: 'cs_30d030a4f3d6a1e132a9b0bdb8fc35f0b81171c7',
 };
 
 /*
