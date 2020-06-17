3dd58bd1c8ce764f7c01680f3a310786767f7914
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-06-17 22:02:17 +0800

    router is needed

--- src/app/product-details/product-details.module.ts
+++ src/app/product-details/product-details.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ProductDetailsPage } from './product-details.page';
 
+import { ProductDetailsPageRoutingModule } from './product-details-routing.module';
+
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ProductDetailsPageRoutingModule
   ],
   declarations: [ProductDetailsPage]
 })
