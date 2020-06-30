104f7134fdc1f6ec35ff99f3e3843d18a3ea797e
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-06-30 22:32:48 +0800

    menu component, issue with ionic wrapper still, need to fix

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+import WooCommerceRestApi from "woocommerce-api";
+
+@Component({
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.scss"],
+})
+export class MenuComponent {
+  constructor() {}
+}
