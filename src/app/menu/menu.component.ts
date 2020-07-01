083dfb6483ad0ec2dece14e79e909cf2eaaf94b5
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-07-01 19:41:35 +0800

    load categories from woo db

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { environment } from "../../environments/environment";
 
 import WooCommerceRestApi from "woocommerce-api";
 
@@ -7,6 +9,31 @@ import WooCommerceRestApi from "woocommerce-api";
   templateUrl: "./menu.component.html",
   styleUrls: ["./menu.component.scss"],
 })
-export class MenuComponent {
-  constructor() {}
+export class MenuComponent implements OnInit {
+  WooCommerce: any;
+  categories: any[];
+
+  constructor() {
+    this.WooCommerce = new WooCommerceRestApi({
+      url: environment.apiUrl,
+      consumerKey: environment.consumerKey,
+      consumerSecret: environment.consumerSecret,
+      version: "wc/v3",
+      wpAPI: true,
+      wpAPIPrefix: "wp-json",
+      verifySsl: environment.verifySSL,
+      queryStringAuth: true,
+    });
+  }
+
+  ngOnInit() {
+    this.WooCommerce.getAsync("products/categories")
+      .then((response) => {
+        this.categories = JSON.parse(response.body);
+      })
+      .catch((error) => {
+        console.log("Error Data:", error);
+      })
+      .finally(() => {});
+  }
 }
