10a374025f22e76db745b711e38cb77fec7957b5
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-07-12 03:32:11 +0800

    component call adjustment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouteReuseStrategy } from "@angular/router";
 
-import { MenuComponent } from "./menu/menu.component";
-import { FloatingCartComponent } from "./floating-cart/floating-cart.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import { FloatingCartComponent } from "./components/floating-cart/floating-cart.component";
 
 import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
 import { SplashScreen } from "@ionic-native/splash-screen/ngx";
