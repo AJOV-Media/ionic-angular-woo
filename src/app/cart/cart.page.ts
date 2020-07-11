c8859bc93c9811820e7b026bef0e91bda1b0e659
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-07-11 12:14:34 +0800

    refactor seperate component page to component blocks

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,10 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-cart",
   templateUrl: "cart.page.html",
   styleUrls: ["cart.page.scss"],
 })
-export class CartPage {
+export class CartPage implements OnInit {
   constructor() {}
+  ngOnInit() {}
+
+  getCartProducts() {
+    let retrieveCartObjects;
+
+    retrieveCartObjects = localStorage.getItem("wooAngularCart");
+    let cartObjects = JSON.parse(retrieveCartObjects || "[]");
+
+    if (cartObjects.length > 0) {
+    }
+  }
 }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 
-import { environment } from "../../environments/environment";
+import { environment } from "../../../environments/environment";
 
 import WooCommerceRestApi from "woocommerce-api";
 
