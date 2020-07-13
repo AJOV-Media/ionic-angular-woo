5d2dcfeb437ebb6e1bba7a2cad6fe17126dae50f
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-07-13 22:13:29 +0800

    product-items updates

--- src/app/components/product-items/product-items.component.ts
+++ src/app/components/product-items/product-items.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 
+import { NavController } from "@ionic/angular";
+
 @Component({
   selector: "app-product-items",
   templateUrl: "./product-items.component.html",
@@ -8,7 +10,35 @@ import { Component, OnInit, Input } from "@angular/core";
 export class ProductItemsComponent implements OnInit {
   @Input() productItems: any[];
 
-  constructor() {}
+  constructor(public navCtrl: NavController) {}
 
   ngOnInit() {}
+
+  mapImage(image) {
+    let imagePath = "";
+
+    if (image === undefined) {
+      imagePath = "noimagepath";
+    } else {
+      //Get only featured image
+      let imageName = image.src.split("/").slice(-1)[0];
+      let imageExplode = imageName.split(".");
+      let imageRename = imageExplode[0] + "-100x100." + imageExplode[1];
+
+      let index = image.src.split("/").indexOf(imageName);
+
+      let urlArray = image.src.split("/");
+
+      urlArray.splice(index, 1);
+
+      let finalUrl = urlArray.join("/") + "/" + imageRename;
+
+      imagePath = finalUrl;
+    }
+
+    return imagePath;
+  }
+  openProductPage(product) {
+    this.navCtrl.navigateForward("/tabs/product-details/" + product);
+  }
 }
