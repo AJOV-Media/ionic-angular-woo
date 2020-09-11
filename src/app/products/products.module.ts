c77951d95498f4bda916a3ba120f377197af1a5b
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-09-11 17:19:19 +0800

    update product page to use product-item components

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,18 +1,14 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { ProductsPage } from './products.page';
+import { IonicModule } from "@ionic/angular";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { ProductsPage } from "./products.page";
 
-import { ProductsPageRoutingModule } from './products-routing.module';
+import { ProductsPageRoutingModule } from "./products-routing.module";
+import { ProductItemsComponent } from "../components/product-items/product-items.component";
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ProductsPageRoutingModule
-  ],
-  declarations: [ProductsPage]
+  imports: [IonicModule, CommonModule, FormsModule, ProductsPageRoutingModule],
+  declarations: [ProductsPage, ProductItemsComponent],
 })
 export class ProductsPageModule {}
